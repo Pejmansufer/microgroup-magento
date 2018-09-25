738bd7bf3fb934a7697538d7d9c4ef6d7c5e8bc3
Pj Sufer <pjseniordev@gmail.com>
2018-09-24 18:21:17 -0700

    Link Update

--- src/httpdocs/js/infomodusupslabel.js
+++ src/httpdocs/js/infomodusupslabel.js
@@ -1,18 +1,1 @@
-window.onload = function () {
-    if (!document.getElementById("upslabel_create")) {
-        var p = document.createElement("P"), tagick = document.createElement("INPUT"), label = document.createElement("LABEL");
-        tagick.setAttribute("id", "upslabel_create");
-        tagick.setAttribute("name", "shipment[upslabel_create]");
-        tagick.setAttribute("type", "checkbox");
-        tagick.value = 1;
-
-        label.setAttribute("for", "upslabel_create");
-        label.setAttribute("name", "shipment[upslabel_create]");
-        label.innerHTML = "Create UPS label ";
-        p.appendChild(label);
-        p.appendChild(tagick);
-        if (document.getElementsByClassName("order-totals-bottom")) {
-            document.getElementsByClassName("order-totals-bottom")[0].insertBefore(p, document.getElementsByClassName("order-totals-bottom")[0].getElementsByClassName("a-right")[0]);
-        }
-    }
-}
+../.modman/ups-sl-manager-pro/js/infomodusupslabel.js
